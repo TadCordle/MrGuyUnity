a1467e1687611c7fbe32170e18a3e62a7343f355
Tad Cordle <tcordle1@umbc.edu>
2015-09-05 00:49:24 -0400

    Tweak movement on slopes

--- Mr Guy/Assets/GuyPhysics.cs
+++ Mr Guy/Assets/GuyPhysics.cs
@@ -3,12 +3,12 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 
-public class GuyPhysics : MonoBehaviour 
+public class GuyPhysics : MonoBehaviour
 {
-    public const float MOVE_ACCEL_GROUND = 90f;
-    public const float MAX_HSPEED_GROUND = 8f;
-    public const float MAX_HSPEED_AIR = 5f;
-    public const float JUMP_POWER = 15f;
+    public const float MOVE_ACCEL_GROUND = 100f;
+    public const float MAX_HSPEED_GROUND = 9f;
+    public const float MAX_HSPEED_AIR = 6f;
+    public const float JUMP_POWER = 17f;
 
     private Rigidbody2D rigidbody;
     private Transform transform;
@@ -16,8 +16,11 @@ public class GuyPhysics : MonoBehaviour
     private CircleCollider2D collider_feet;
     private BoxCollider2D collider_torso;
 
+    private bool movingLeft, movingRight;
+
     private bool onGround;
-    private float currGroundNormal;
+    private bool rightTouchingGround, leftTouchingGround;
+    private Vector2 currGroundDir;
 
 	// Use this for initialization
 	void Awake () 
@@ -31,12 +34,16 @@ public class GuyPhysics : MonoBehaviour
     void Start()
     {
         onGround = OnGround();
+
+        movingLeft = false;
+        movingRight = false;
     }
 	
 	// Update is called once per frame
 	void Update ()
     {
         onGround = OnGround();
+        collider_feet.sharedMaterial.friction = onGround ? 0.1f : 0f;
 
         float closestRotation = transform.localRotation.eulerAngles.z;
         rigidbody.MoveRotation(Mathf.Lerp(closestRotation > 180 ? closestRotation - 360 : closestRotation, 0, 0.5f));
@@ -61,21 +68,25 @@ public class GuyPhysics : MonoBehaviour
 
     public void MoveLeft()
     {
+        movingLeft = true;
         rigidbody.drag = 0;
         if (rigidbody.velocity.x > -(onGround ? MAX_HSPEED_GROUND : MAX_HSPEED_AIR))
-            rigidbody.AddForce(Vector2.left * MOVE_ACCEL_GROUND);
+            rigidbody.AddForce(((leftTouchingGround || onGround) && currGroundDir.y / currGroundDir.x <= 0.9f ? -currGroundDir * Mathf.Pow(currGroundDir.x, 4) : Vector2.left) * MOVE_ACCEL_GROUND);
     }
 
     public void MoveRight()
     {
+        movingRight = true;
         rigidbody.drag = 0;
         if (rigidbody.velocity.x < (onGround ? MAX_HSPEED_GROUND : MAX_HSPEED_AIR))
-            rigidbody.AddForce(Vector2.right * MOVE_ACCEL_GROUND);
+            rigidbody.AddForce(((rightTouchingGround || onGround) && currGroundDir.y / currGroundDir.x >= -0.9f ? currGroundDir * Mathf.Pow(currGroundDir.x, 4) : Vector2.right) * MOVE_ACCEL_GROUND);
     }
 
     public void StopMoving()
     {
-        rigidbody.velocity = Vector2.Lerp(rigidbody.velocity, rigidbody.velocity - Vector2.right * rigidbody.velocity.x, 0.4f);
+        movingLeft = false;
+        movingRight = false;
+        rigidbody.velocity = Vector2.Lerp(rigidbody.velocity, rigidbody.velocity - Vector2.right * rigidbody.velocity.x, (onGround ? 0.7f : 0.3f));
     }
 
     public void Jump()
@@ -93,22 +104,27 @@ public class GuyPhysics : MonoBehaviour
         RaycastHit2D[] hitsRight = Physics2D.RaycastAll((Vector2)collider_feet.transform.position + Vector2.right * 0.5f, Vector2.down, 0.9f);
         RaycastHit2D[] hits = hitsMiddle.Union(hitsLeft).Union(hitsRight).ToArray<RaycastHit2D>();
 
-        Debug.Log(hitsLeft.Length + "  " + hitsMiddle.Length + "  " + hitsRight.Length);
-
         int groundCount = 0;
         float normals = float.MaxValue;
+        leftTouchingGround = rightTouchingGround = false;
+        currGroundDir = Vector2.right;
         foreach (RaycastHit2D h in hits)
         {
             if (h.collider.gameObject.CompareTag("Ground")) // TODO: gonna need a way to unify tags of things we can stand on
             {
                 groundCount++;
+                if (hitsLeft.Contains(h))
+                    leftTouchingGround = true;
+                if (hitsRight.Contains(h))
+                    rightTouchingGround = true;
+
                 if (Mathf.Abs(h.normal.x / h.normal.y) < normals)
                 {
                     normals = Mathf.Abs(h.normal.x / h.normal.y);
-                    currGroundNormal = normals;
+                    currGroundDir = new Vector2(h.normal.normalized.y, -h.normal.normalized.x);
                 }
             }
         }
-        return groundCount > 0 && normals < 1.3f;
+        return groundCount > 0 && normals < 1.6f;
     }
 }
